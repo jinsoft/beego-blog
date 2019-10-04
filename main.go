00a6efa5f3545e382e631d4327149f3a55589407
ainiok <job@ainiok.com>
2019-10-05 02:27:07 +0800

    [ADD] 增加错误定制页面

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"web/controllers/admin"
 	_ "web/routers"
 )
 
 func main() {
+	beego.ErrorController(&admin.ErrorController{})
 	beego.BConfig.WebConfig.Session.SessionOn = true
 	beego.Run()
 }
